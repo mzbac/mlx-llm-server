974ab06e50296a4491ae9f9f0e71d79674acb9a0
Anchen <li.anchen.au@gmail.com>
2024-01-14 22:47:09 +1100

    chore: update inference script

--- inference.py
+++ inference.py
@@ -1,5 +1,51 @@
+import argparse
 from mlx_lm import load, generate
 
-model, tokenizer = load("mistralai/Mistral-7B-v0.1")
+DEFAULT_PROMPT = "Hello, world!"
+DEFAULT_MAX_TOKENS = 100
+DEFAULT_TEMP = 0.7
 
-response = generate(model, tokenizer, prompt="hello", verbose=True)
+def main():
+    parser = argparse.ArgumentParser(description="Generate text using a language model")
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="mlx_model",
+        help="The path to the local model directory or Hugging Face repo."
+    )
+    parser.add_argument(
+        "--prompt",
+        default=DEFAULT_PROMPT,
+        help="Message to be processed by the model"
+    )
+    parser.add_argument(
+        "--max-tokens",
+        "-m",
+        type=int,
+        default=DEFAULT_MAX_TOKENS,
+        help="Maximum number of tokens to generate"
+    )
+    parser.add_argument(
+        "--temp",
+        type=float,
+        default=DEFAULT_TEMP,
+        help="Sampling temperature"
+    )
+
+    args = parser.parse_args()
+
+    model, tokenizer = load(args.model)
+    response = generate(
+        model=model,
+        tokenizer=tokenizer,
+        prompt=args.prompt,
+        temp=args.temp,
+        max_tokens=args.max_tokens,
+        verbose=True
+    )
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
